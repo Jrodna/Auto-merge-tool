a342741475837a8d2ad98b8cae3237fcafae65c0
Jordan Blackbourn <jordan.blackbourn@cascade.app>
2021-12-22 10:43:10 +1300

    Added difftool

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {execGitCmd} from "run-git-command";
-import { getVSCodeDownloadUrl } from '@vscode/test-electron/out/util';
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('auto-merge-tool.mergetool', () => {
+	let mergetool = vscode.commands.registerCommand('auto-merge-tool.mergetool', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Opening the mergetool');
@@ -23,8 +22,22 @@ export function activate(context: vscode.ExtensionContext) {
 
     terminal.sendText('git mergetool', true);
 	});
+  context.subscriptions.push(mergetool);
 
-	context.subscriptions.push(disposable);
+  let difftool = vscode.commands.registerCommand('auto-merge-tool.difftool', () => {
+    vscode.window.showInformationMessage('Opening the difftool');
+    const terminal: vscode.Terminal = vscode.window.createTerminal('auto-merge-tool');
+
+    terminal.sendText('git difftool', true);
+	});
+
+  context.subscriptions.push(difftool);
+
+  vscode.window.registerUriHandler({
+    handleUri(uri:vscode.Uri) {
+        // do something with the URI
+    }
+});
 }
 
 // this method is called when your extension is deactivated
