c292af09ada971fe7e309ccd9779ecf6c6d93367
Jordan Blackbourn <jordan.blackbourn@cascade.app>
2021-12-21 17:14:27 +1300

    Can open mergetool with command

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {execGitCmd} from "run-git-command";
+import { getVSCodeDownloadUrl } from '@vscode/test-electron/out/util';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,10 +15,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('auto-merge-tool.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('auto-merge-tool.mergetool', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Auto Merge Tool!');
+		vscode.window.showInformationMessage('Opening the mergetool');
+    const terminal: vscode.Terminal = vscode.window.createTerminal('auto-merge-tool');
+
+    terminal.sendText('git mergetool', true);
 	});
 
 	context.subscriptions.push(disposable);
